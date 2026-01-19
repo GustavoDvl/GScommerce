b09107789cb5832b5efc3ce92aea6043cba437c6
GustavoDvl <124375726+GustavoDvl@users.noreply.github.com>
2026-01-19 09:43:57 -0300

    cria o product repository

--- src/main/java/com/devsuperior/gscommerce/repositories/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.gscommerce.repositories;
-
-import com.devsuperior.gscommerce.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<User, Long> {
-}
